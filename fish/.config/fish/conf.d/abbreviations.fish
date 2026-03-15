8fa4027ad6d05bbb4a7c773c6a012a3675753b35
Aditya Deshlahre <wymaditya@gmail.com>
2026-03-15 16:44:16 +0530

    chore: fix abbreviations fish

--- fish/.config/fish/conf.d/abbreviations.fish
+++ fish/.config/fish/conf.d/abbreviations.fish
@@ -4,20 +4,15 @@
 #  |  |_)|   |
 #
 
-abbr -a cl clear
-abbr -a dockerps docker ps
-abbr -a ga git add
-abbr -a lg lazygit
-abbr -a gcm git commit -m
-abbr -a gss git status
-abbr -a l ls -Alh --color=auto # [A]lmost all (except . && ..), [l]ist, [h]: display unit
 abbr -a nv neovide --fork
 abbr -a v nvim
-abbr -a weather curl 'https://wttr.in'
+abbr -a dockerps docker ps
+abbr -a lg lazygit
 
 # .. to cd .., ... to cd ../.., etc.
 function multicd
     echo cd (string repeat -n (math (string length -- $argv[1]) - 1) ../)
 end
 abbr --add dotdot --regex '^\.\.+$' --function multicd
+
 
